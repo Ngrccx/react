7d3e81c9a9342872284bcc15a23f508531645d8d
Ngrccx <93627469+Ngrccx@users.noreply.github.com>
2022-05-16 21:51:38 -0300

    Desafio II

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            Hola soy Mishka
+            <ul className="nav__items">
+                <li>Inicio</li>
+                <li>Sobre Nosotros</li>
+                <li>Tienda</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
